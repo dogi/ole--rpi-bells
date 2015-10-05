afff47ec49e55634bd9eff366410139f3a4bf716
dogi <dogi@sugarlabs.org>
2015-10-04 23:39:14 -1000

    pseudo code in form of comments added

--- qa/autorun.sh
+++ qa/autorun.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# default settings
+
+# load local settings from qa.config file
+
+# set rpi network
+
+# git pull to get newest code, if not here create
+# cp new autorun.sh over myself
+
+# download qa content or check by sha1 if there is newer
+
+# start one of the 3 scripts
