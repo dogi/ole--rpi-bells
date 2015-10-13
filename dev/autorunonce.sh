d7ca1132ab747b53c7d5c68a72d11ac21cc95dc6
dogi <dogi@sugarlabs.org>
2015-10-13 03:51:31 -0400

    security level improvment ;P

--- dev/autorunonce.sh
+++ dev/autorunonce.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 pirateship rename dev
+
+# default settings
+password=''
+
+function pass {
+passwd pi << EOF
+$1
+$1
+EOF
+}
+
+# set password
+pass $password
+
 pirateship ethernet 204.9.221.59 255.255.255.128 204.9.221.1 "204.9.221.30 204.9.223.18 204.9.223.19"
