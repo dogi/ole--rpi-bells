e2f4989aca223d0ea34abbcd9a9d7a02c28dd1b4
dogi <dogi@sugarlabs.org>
2015-10-14 03:54:20 -0400

    2th ;P

--- dev/autorunonce.sh
+++ dev/autorunonce.sh
@@ -13,6 +13,8 @@ EOF
 }
 
 # set password
-pass $password
+if [[ $password != "" ]] ; then
+  pass $password
+fi
 
 pirateship ethernet 204.9.221.59 255.255.255.128 204.9.221.1 "204.9.221.30 204.9.223.18 204.9.223.19"
