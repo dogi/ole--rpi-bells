22e03bfaad997b79fb093841281364a020ba22d9
dogi <dogi@sugarlabs.org>
2015-10-18 06:45:05 -0400

    version 0.11.69

--- qa/2newcommunities-autorunonce.sh
+++ qa/2newcommunities-autorunonce.sh
@@ -119,7 +119,7 @@ echo '#!/bin/sh' > /boot/autorun.sh
 echo '' >> /boot/autorun.sh
 
 community old 5984 63
-community new 5985 68
+community new 5985 69
 
 # write simple webpage with links
 echo '#!/usr/bin/env node' > /root/ole/server.js
