0d4ffe362d3b8793426836b7efd484e0b0e948a5
dogi <stefan@unterhauser.name>
2016-05-19 06:45:58 -0400

    version 0.12.15

--- qa/2nations-autorunonce.sh
+++ qa/2nations-autorunonce.sh
@@ -124,7 +124,7 @@ echo '' >> /boot/autorun.sh
 
 # install an old and a new nation
 nation old 5984 0.12.9
-nation new 5985 0.12.14
+nation new 5985 0.12.15
 
 
 
--- qa/2oldcommunities-autorunonce.sh
+++ qa/2oldcommunities-autorunonce.sh
@@ -74,7 +74,7 @@ echo '#!/bin/sh' > /boot/autorun.sh
 echo '' >> /boot/autorun.sh
 
 community old 5984 0.12.9
-community new 5985 0.12.14
+community new 5985 0.12.15
 
 # write simple webpage with links
 echo '#!/usr/bin/env node' > /root/ole/server.js
