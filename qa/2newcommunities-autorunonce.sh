02b65c9d1ba9b4bdb546b04ab53c655d3457a226
dogi <stefan@unterhauser.name>
2016-06-13 20:27:11 -0400

    version 0.12.21

--- qa/2newcommunities-autorunonce.sh
+++ qa/2newcommunities-autorunonce.sh
@@ -79,7 +79,7 @@ echo '#!/bin/sh' > /boot/autorun.sh
 echo '' >> /boot/autorun.sh
 
 community old 5984 0.12.16
-community new 5985 0.12.20
+community new 5985 0.12.21
 
 # write simple webpage with links
 echo '#!/usr/bin/env node' > /root/ole/server.js
