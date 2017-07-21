ef6479bd17c465d0cfd131805abefb37d0dace65
dogi <stefan@unterhauser.name>
2017-07-21 03:42:53 -0400

    version 0.13.7

--- qa/2nations-autorunonce.sh
+++ qa/2nations-autorunonce.sh
@@ -164,7 +164,7 @@ echo '' >> /boot/autorun.sh
 
 # install an old and a new nation
 nation old 5984 0.12.59
-nation new 5985 0.13.6
+nation new 5985 0.13.7
 
 #center
 docker run -d -p 5983:5984 --name center -v /srv/data/center:/usr/local/var/lib/couchdb -v /srv/log/center:/usr/local/var/log/couchdb dogi/rpi-couchdb
